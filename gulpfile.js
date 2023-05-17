e94016d8fd2ca9c50a815bec733ff1ff5d439d65
Fenix4088 <fenix4088@gmail.com>
2023-05-17 16:37:05 +0300

    src dir

--- gulpfile.js
+++ gulpfile.js
@@ -7,34 +7,37 @@ const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 const inlineCss = require('gulp-inline-css');
 
+
+const PATH = {
+    SCSS_DIR: './src/scss/**/*.scss',
+}
+
 function buildStyles() {
-      return gulp.src('./scss/**/*.scss')
-      //   .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-      //   .pipe(sourcemaps.write('./maps'))
-      .pipe(autoprefixer({
-            cascade: false
-      }))
-        .pipe(concat('bundle.css'))
-        .pipe(gulp.dest('./dist/css'));
+      return gulp.src(PATH.SCSS_DIR)
+            .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+            .pipe(autoprefixer({
+                  cascade: false
+            }))
+            .pipe(concat('bundle.css'))
+            .pipe(gulp.dest('./dist/css'));
     };
 
 
 function convertInline() {
       return gulp.src('./*.html')
-      .pipe(inlineCss({
-                  applyStyleTags: true,
-                  applyLinkTags: true,
-                  removeStyleTags: true,
-                  removeLinkTags: true
-      }))
-      .pipe(gulp.dest('dist/'));
+            .pipe(inlineCss({
+                        applyStyleTags: true,
+                        applyLinkTags: true,
+                        removeStyleTags: true,
+                        removeLinkTags: true
+            }))
+            .pipe(gulp.dest('dist/'));
 }
 
 exports.buildStyles = buildStyles;
 exports.convertInline = convertInline;
 
 exports.watch = function async () {
-      gulp.watch('./scss/**/*.scss', buildStyles);
+      gulp.watch(PATH.SCSS_DIR, buildStyles);
       gulp.watch('./dist/css/bundle.css', convertInline);
 };
