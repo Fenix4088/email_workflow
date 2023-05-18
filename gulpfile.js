a7ffcc71d127313ec93bffd0c50fa47c2b8abbde
Fenix4088 <fenix4088@gmail.com>
2023-05-18 10:30:48 +0300

    added pug

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,19 @@ const sourcemaps = require('gulp-sourcemaps');
 const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 const inlineCss = require('gulp-inline-css');
+const pug = require('gulp-pug')
 
 
 const PATH = {
     SCSS_DIR: './src/scss/**/*.scss',
+    PUG_FILES: "src/*.pug"
+}
+
+function html() {
+      return gulp.src(PATH.PUG_FILES)
+            .pipe(pug({pretty: true}))
+            .pipe(gulp.dest("dist"))
+
 }
 
 function buildStyles() {
@@ -20,7 +29,7 @@ function buildStyles() {
             }))
             .pipe(concat('bundle.css'))
             .pipe(gulp.dest('./dist/css'));
-    };
+};
 
 
 function convertInline() {
@@ -36,6 +45,7 @@ function convertInline() {
 
 exports.buildStyles = buildStyles;
 exports.convertInline = convertInline;
+exports.html = html;
 
 exports.watch = function async () {
       gulp.watch(PATH.SCSS_DIR, buildStyles);
