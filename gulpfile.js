3f1f845fa175265130a955279ee3caf42418926c
Fenix4088 <fenix4088@gmail.com>
2023-05-18 11:24:23 +0300

    improve watch script logic

--- gulpfile.js
+++ gulpfile.js
@@ -11,13 +11,17 @@ const pug = require('gulp-pug')
 
 const PATH = {
     SCSS_DIR: './src/scss/**/*.scss',
-    PUG_FILES: "src/*.pug"
+//     PUG_FILES: "src/*.pug"
+      PUG_FILES: "src/emails/**/*.pug",
+      PUG_COMPONENTS: "src/components/**/*.pug"
 }
 
 function html() {
       return gulp.src(PATH.PUG_FILES)
             .pipe(pug({pretty: true}))
-            .pipe(gulp.dest("dist"))
+            // .pipe(gulp.dest("dist"))
+            .pipe(gulp.dest("src/html"))
+
 
 }
 
@@ -28,12 +32,12 @@ function buildStyles() {
                   cascade: false
             }))
             .pipe(concat('bundle.css'))
-            .pipe(gulp.dest('./dist/css'));
+            .pipe(gulp.dest('./src/css'));
 };
 
 
 function convertInline() {
-      return gulp.src('./*.html')
+      return gulp.src('./src/html/**/*.html')
             .pipe(inlineCss({
                         applyStyleTags: true,
                         applyLinkTags: true,
@@ -41,6 +45,7 @@ function convertInline() {
                         removeLinkTags: true
             }))
             .pipe(gulp.dest('dist/'));
+
 }
 
 exports.buildStyles = buildStyles;
@@ -49,5 +54,6 @@ exports.html = html;
 
 exports.watch = function async () {
       gulp.watch(PATH.SCSS_DIR, buildStyles);
-      gulp.watch('./dist/css/bundle.css', convertInline);
+      gulp.watch([PATH.PUG_FILES, PATH.PUG_COMPONENTS], html);
+      gulp.watch(['./src/html/**/*.html', './src/css/bundle.css'], convertInline);
 };
